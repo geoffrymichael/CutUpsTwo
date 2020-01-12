94258fc25b4e65da895d955681c6aa2426293c4c
Geoffry Gambling <geoffrymichael@gmail.com>
2020-01-12 13:00:48 -0800

    Initial Commit

---
+++ CutUpsTwo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CutUpsTwo
+//
+//  Created by Geoffry Gambling on 1/12/20.
+//  Copyright © 2020 Geoffry Gambling. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
